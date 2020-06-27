6be405541a175f2813fa91316fda49676c0e1992
azharali8907 <60216443+azharali8907@users.noreply.github.com>
2020-06-27 03:28:31 -0700

    update App.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
-function App() {
+function  App() {
+  //let data = {title: "wating for data"};
+  const [todo, setTodo] = useState([{}]);
+  //const [isData, setData] = useState(false);
+  //const [isFetch, setFetch] = useState(false);
+
+    useEffect(() => {
+      async function fetchData() {
+        //setFetch(true);
+        const response = await fetch("https://api.github.com/users/azharali8907/repos");
+      console.log("response = ",response);
+      //setFetch(false);
+      setTodo(await response.json());
+      console.log("Data = ", todo);
+      }
+      fetchData();
+    },[]);
+
+    /* if(isFetch){
+      return "Data Loding..."
+    } */
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>You are seeing all repositories</h1>
+    <ul>
+      {todo.map((repoObj, ind) => {
+        return (<li key={ind}>{repoObj.name}</li>)
+      })}
+    </ul>
+  </div>
   );
 }
 
